385e70039f14d6e0c88abbbd097794bb345d9f15
Ítalo <italooo.senai@gmail.com>
2025-04-02 10:16:22 -0300

    corrigindo erro do dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
