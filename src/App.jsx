e74c6e3ed467bb3c2f613081b09352f1c463faee
Ítalo <italooo.senai@gmail.com>
2025-04-08 07:59:52 -0300

    Merge branch 'main' into Ítalo

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import "./App.css";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import Logo from "./assets/LogotipoTotalHealth.webp"; // Logo inserida
+import Logo from "./assets/LogotipoTotalHealth-Photoroom.png"; // Logo inserida
+import LogoMobile from "./assets/LogotipoTotalHealth-Mobile.png";
 
 import Sobre from "./pages/Sobre";
 import Contato from "./pages/Contato";
@@ -20,143 +21,41 @@ import Consulta from "./pages/Consulta";
 import Agendamento from "./pages/Agendamento";
 import Sair from "./pages/Sair";
 import NaoEncontrado from "./pages/NaoEncontrado";
-
-const Home = () => {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        {/* Barra lateral */}
-        <div className="col-3 bg-dark text-white vh-100 p-4">
-          <div className="text-center mb-4">
-            <img
-              src={Logo}
-              alt="TotalHealth Logo"
-              className="img-fluid"
-              style={{ maxWidth: "150px" }}
-            />
-          </div>
-          <ul className="nav flex-column">
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/usuario">
-                Usuário
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/prescricao">
-                Prescrições
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/pagamento">
-                Pagamentos
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/medicoEspecialidade">
-                Médico/Especialidades
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/medico">
-                Médicos
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/exame">
-                Exames
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/especialidade">
-                Especialidades
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/contato">
-                Contato
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/consulta">
-                Consultas
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="/agendamento">
-                Agendamentos
-              </a>
-            </li>
-          </ul>
-          <div className="mt-4">
-            {/* <a className="nav-link text-white" href="/perfil">
-              Perfil
-            </a> */}
-            <a className="nav-link text-white" href="/sair">
-              Sair
-            </a>
-            <p className="mt-3">Linha direta de emergência</p>
-            <p className="fw-bold">+91 - 999 999 9999</p>
-          </div>
-        </div>
-
-        {/* Conteúdo principal */}
-        <div className="col-9 p-5">
-          <h2>Nova Prescrição</h2>
-          <form className="mt-4">
-            <div className="mb-3">
-              <label className="form-label">Consulta</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Digite a consulta"
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Valor</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Digite o valor"
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Descrição</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Digite a descrição"
-              />
-            </div>
-            <button className="btn btn-primary">Criar</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-};
+import NovaConsulta from "./pages/NovaConsulta";
+import Perfil from "./pages/Perfil";
+import Home from "./pages/Home";
+import HeaderMobile from "./components/header/HeaderMobile";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sobre" element={<Sobre />} />
-          <Route path="/contato" element={<Contato />} />
-          <Route path="/usuario" element={<Usuario />} />
-          <Route path="/prescricao" element={<Prescricao />} />
-          <Route path="/pagamento" element={<Pagamento />} />
-          <Route path="/medicoEspecialidades" element={<MedicoEspecialidade />}/>
-          <Route path="/medico" element={<Medico />} />
-          <Route path="/exame" element={<Exame />} />
-          <Route path="/especialidade" element={<Especialidade />} />
-          <Route path="/consulta" element={<Consulta />} />
-          <Route path="/agendamento" element={<Agendamento />} />
-          <Route path="/sair" element={<Sair />} />
-          <Route path="*" element={<NaoEncontrado />} />
-        </Routes>
-      </main>
+      <HeaderMobile Logo={LogoMobile} />
+      <div className="d-flex min-vh-100">
+        <Header Logo={Logo} />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/usuario" element={<Usuario />} />
+            <Route path="/prescricao" element={<Prescricao />} />
+            <Route path="/pagamento" element={<Pagamento />} />
+            <Route
+              path="/medicoEspecialidades"
+              element={<MedicoEspecialidade />}
+            />
+            <Route path="/medico" element={<Medico />} />
+            <Route path="/exame" element={<Exame />} />
+            <Route path="/especialidade" element={<Especialidade />} />
+            <Route path="/consulta" element={<Consulta />} />
+            <Route path="/agendamento" element={<Agendamento />} />
+            <Route path="/sair" element={<Sair />} />
+            <Route path="*" element={<NaoEncontrado />} />
+            <Route path="/consulta/nova" element={<NovaConsulta />} />
+            <Route path="/perfil" element={<Perfil />} />
+          </Routes>
+        </main>
+      </div>
       <Footer />
     </BrowserRouter>
   );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white text-center py-3 mt-5">
+    <footer className="bg-dark text-white text-center py-3">
       <div className="container">
         <p className="mb-0">
           © 2025 TotalHealth. Todos os direitos reservados.
